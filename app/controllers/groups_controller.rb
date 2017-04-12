2cbf1bf54772463d388b4019049f53d436bd7fcc
赵元平 <bunnybubby@sina.com.cn>
2017-04-12 22:48:03 +0800

    implement groups#show

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,6 +2,9 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
+  def show
+    @group = Group.find(params[:id])
+  end
   def new
     @group = Group.new
   end
@@ -11,7 +14,7 @@ class GroupsController < ApplicationController
 
     redirect_to groups_path
   end
-  
+
   private
   def group_params
     params.require(:group).permit(:title, :description)
